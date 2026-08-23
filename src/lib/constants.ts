f3d8e547df2864a6dc9ef0629f660f8f6b1e5c08
sEEGplan <serdarakkol@hotmail.com>
2026-08-23 09:48:46 -0400

    v1.2

--- src/lib/constants.ts
+++
@@ -1,4 +0,0 @@
-// Reference pixel dimensions of brain-template.png -- the master coordinate system.
-// All normalized (0..1) electrode/sketch coordinates are relative to this.
-export const REF_W = 1770;
-export const REF_H = 1281;
--- src/lib/export/png.ts
+++
@@ -1,9 +0,0 @@
-import { captureNode } from "./capture";
-
-export async function exportWorkspacePng(node: HTMLElement, filename: string) {
-  const dataUrl = await captureNode(node, 3);
-  const link = document.createElement("a");
-  link.href = dataUrl;
-  link.download = filename.endsWith(".png") ? filename : `${filename}.png`;
-  link.click();
-}
